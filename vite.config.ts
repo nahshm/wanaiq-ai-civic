0b743470b8657e37321b5789897808ebf325e47b
nahshm <nahshm@gmail.com>
2026-03-13 06:01:40 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,20 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: process.env.VITE_BASE_PATH || "/wanaiq-ai-civic",
+
+  // Source - https://stackoverflow.com/a/69041080
+// Posted by flydev, modified by community. See post 'Timeline' for change history
+// Retrieved 2026-03-13, License - CC BY-SA 4.0
+
+const ASSET_URL = process.env.VITE_BASE_PATH || '';
+
+export default { 
+  base: `${VITE_BASE_PATH}/dist/`,
+
+  
+},
+
+
   plugins: [
     react(),
     mode === 'development' &&
